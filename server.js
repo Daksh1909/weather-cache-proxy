d598be877d855db7b8c2c81a4bc161408c951b88
Daksh Kumar Sahu <daksh.sahu19@gmail.com>
2025-11-02 14:42:45 +0530

    app can run via terminal and port should be given with it

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
+#!/usr/bin/env node
+
 const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
 
-const PORT = process.env.PORT || 5000;
+const { Command } = require('commander') 
+const program = new Command();
+
+program
+    .name('weather-app')
+    .description('Start the server')
+    .option('-p, --port <number>', 'Port number to run the server on', process.env.PORT ||'3000')
+    .parse(process.argv);
+
+const options = program.opts();
+const PORT = parseInt(options.port, 10);
+// const PORT = process.env.PORT || 5000;
 
 app.set('trust proxy', 1);
 
+app.use(cors());
 app.use(express.static('public'))
 
 app.use('/api', require('./routes/index'));
 
-app.use(cors());
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}.`))
